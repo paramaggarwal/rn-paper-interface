8df7bcc622f5cc32ddbc6954d5da7564de34c7d2
Param Aggarwal <paramaggarwal@gmail.com>
2016-07-21 22:55:50 +0530

    Update style templates

--- app/feed/types.js
+++ app/feed/types.js
@@ -3,21 +3,25 @@ let types = {
     title: 'PHOTO',
     colorDark: 'black',
     colorLight: 'white',
+    reaction: '📷'
   },
   'question': {
     title: 'QUESTION',
     colorDark: '#00301B',
     colorLight: '#00AA4E',
+    reaction: '🤓'
   },
   'deadline': {
     title: 'DEADLINE',
     colorDark: '#A91400',
     colorLight: '#FF5505',
+    reaction: '😱'
   },
   'update': {
     title: 'UPDATE',
     colorDark: '#032269',
     colorLight: '#27AFFF',
+    reaction: '👏'
   }
 };
 
